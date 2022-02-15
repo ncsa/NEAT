58855bf08e6be1a2b9f04831f4ecb0110e8b0200
joshfactorial <jallen17@illinois.edu>
2022-02-15 15:00:40 -0600

    Renamed model files as .dat, and they are all gzipped now for space considerations. Updated the code so it can open gzipped files.
    Former-commit-id: 8ca6fb49ec53d9f4dde025a32bc42e164d0f8947

--- gen_reads.py
+++ gen_reads.py
@@ -203,7 +203,7 @@ def main(raw_args=None):
     # sequencing error model
     if se_model is None:
         print('Using default sequencing error model.')
-        se_model = sim_path / 'models/errorModel_toy.p'
+        se_model = sim_path / 'models/errorModel_toy.dat.gz'
         se_class = ReadContainer(read_len, se_model, se_rate, rescale_qual)
     else:
         # probably need to do some sanity checking
@@ -212,7 +212,7 @@ def main(raw_args=None):
     # GC-bias model
     if gc_bias_model is None:
         print('Using default gc-bias model.')
-        gc_bias_model = sim_path / 'models/gcBias_toy.p'
+        gc_bias_model = sim_path / 'models/gcBias_toy.dat.gz'
         try:
             [gc_scale_count, gc_scale_val] = pickle.load(open(gc_bias_model, 'rb'))
         except IOError:
