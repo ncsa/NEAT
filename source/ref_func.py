22785a77cece838a3d1223aa3332db0da6c8c5dd
joshfactorial <jallen17@illinois.edu>
2022-03-14 15:26:30 -0500

    trying to mesh everything together for vcf creation

--- source/ref_func.py
+++ source/ref_func.py
@@ -13,7 +13,7 @@ from source.error_handling import premature_exit, log_mssg
 from source.probability import DiscreteDistribution
 
 
-def find_n_regions(reference) -> dict:
+def find_habitable_regions(reference) -> dict:
     """
     Finds N regions in the sequence
     :param reference: Biopython SeqRecord object containing the sequence to scan.
@@ -32,38 +32,22 @@ def find_n_regions(reference) -> dict:
     {'chr1': [(0, 23)]}
     """
     # data explanation: my_dat[n_atlas[0][0]:n_atlas[0][1]] = solid block of Ns
-    prev_ni = 0
-    n_count = 0
-    n_atlas = {x: [] for x in reference}
-    for chrom in reference:
-        sequence = reference[chrom]
-        for i in range(len(sequence)):
-            if sequence[i] not in ALLOWED_NUCL:
-                if n_count == 0:
-                    prev_ni = i
-                n_count += 1
-                if i == len(sequence) - 1:
-                    n_atlas[chrom].append((prev_ni, prev_ni + n_count))
-            else:
-                if n_count > 0:
-                    n_atlas[chrom].append((prev_ni, prev_ni + n_count))
-                n_count = 0
     non_n_atlas = {x: [] for x in reference}
     for chrom in reference:
-        if n_atlas[chrom]:
-            # See where N's start (usually at 0)
-            if n_atlas[chrom][0][0] > 0:
-                # If N's start after 0, then account for that region here
-                non_n_atlas[chrom].append((0, n_atlas[chrom][0][0]))
-            prev_end = n_atlas[chrom][0][1]
-            # skip the first one since we just dealt with it
-            for n_region in n_atlas[chrom][1:]:
-                non_n_atlas[chrom].append((prev_end, n_region[0]))
-                prev_end = n_region[1]
-            if prev_end < len(reference[chrom]):
-                non_n_atlas[chrom].append((prev_end, len(reference[chrom])))
-        else:
-            non_n_atlas[chrom].append((0, len(reference[chrom])))
+        sequence = reference[chrom]
+        prev_non_ni = 0
+        non_n_count = 0
+        for i in range(len(sequence)):
+            if sequence[i] in ALLOWED_NUCL:
+                if non_n_count == 0:
+                    prev_non_ni = i
+                non_n_count += 1
+                if i == len(sequence) - 1:
+                    non_n_atlas[chrom].append((prev_non_ni, prev_non_ni + non_n_count))
+            else:
+                if non_n_count > 0:
+                    non_n_atlas[chrom].append((prev_non_ni, prev_non_ni + non_n_count))
+                non_n_count = 0
     return non_n_atlas
 
 
@@ -73,10 +57,8 @@ def model_trinucs(reference, models, safe_zones):
     for chrom in reference:
         trinuc_bias = np.zeros(len(reference[chrom]))
         chrom_atlas = safe_zones[chrom]
-        non_n = []
-        prev_end = 0
 
-        for zone in safe_zones:
+        for zone in chrom_atlas:
             # Start at +1 so we get a trinucleotide to start and end one shy for the same reason
             for i in range(zone[0] + 1, zone[1] - 1):
                 trinuc = reference[chrom].seq[i-1:i+2]
@@ -89,6 +71,6 @@ def model_trinucs(reference, models, safe_zones):
 
 
 def process_reference(reference_index, models):
-    allowed_areas = find_n_regions(reference_index)
+    allowed_areas = find_habitable_regions(reference_index)
     trinuc_models = model_trinucs(reference_index, models, allowed_areas)
     return allowed_areas, trinuc_models
