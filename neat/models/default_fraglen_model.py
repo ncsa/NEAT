d389ada333b6411e101583f77fc8d6f60e2cbd90
Joshua Allen <jallen17@illinois.edu>
2024-05-13 22:18:52 -0500

    Making progress simplifying and strengthening the generate reads section

--- neat/models/default_fraglen_model.py
+++
@@ -1,8 +0,0 @@
-"""
-The following model is the default for the fragment length distribution.
-"""
-
-default_fragment_mean = 601.0
-default_fragment_std = 103.0
-default_fragment_max = 1626.0
-default_fragment_min = 3.0
