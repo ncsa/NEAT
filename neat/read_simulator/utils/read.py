83a45c74942e5f70d3a7de9e273c4e9bf62bceda
Zhouyang Lian <zhouyang.lian@familian.life>
2025-01-08 15:31:27 -0800

    convert values in self.quality_array to integer when they are strings

--- neat/read_simulator/utils/read.py
+++ neat/read_simulator/utils/read.py
@@ -341,7 +341,7 @@ class Read:
         # This applies any variants, updates quality score and read sequence in place
         self.apply_variants_for_final_output(qual_model, rng)
 
-        self.read_quality_string = "".join([chr(x + quality_offset) for x in self.quality_array])
+        self.read_quality_string = "".join([chr(int(x) + quality_offset) for x in self.quality_array])
         # If this read isn't low quality, pick a standard mapping quality
         # We could have this be user assigned.
         if not self.mapping_quality:
